5a633d130369d2a60e68ec90b4353c7a09dbf727
frank@ozando.com <ozando#111>
2016-06-13 23:51:05 +1200

    Added GCM Push support

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  push: {
+    android: {
+      senderId: process.env.GCM_SENDER_ID,
+      apiKey: process.env.GCM_API_KEY
+    },
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
