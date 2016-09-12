603e7117c7f23718ccad1c5fc6583b615a5f9edb
frank@ozando.com <ozando#111>
2016-09-12 23:46:47 +1200

    Added test getCarers cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,29 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define("getCarers", function(request,response)
+{
+    var user = new Parse.User();
+    user.id = request.params.clientid;
+    var query = new Parse.Query("Relationship");
+    query.include('clientId');
+    query.equalTo("clientId", user);
+    query.find(
+    {
+        success: function(results)
+        {  var ids = "";
+           for ( var i = 0; i < results.length; ++i )
+           {
+               ids += results[i].get("carerId");
+               ids += ",";
+           }
+           response.success(ids);
+        },
+        error: function(error)
+        {
+           console.log(error)
+           response.error(error);
+        }
+    });
+});
